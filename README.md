3ffbee903fd89742884bf5ccb84938f1cc5f595d
Rafael Sartori <rafael.sartori.vantin@gmail.com>
2024-05-02 17:40:34 -0300

    funcoes funcionando, modelos adicionados na cena

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
-# trabalho-cg
+# Trabalho Computação Gráfica
+
+Projeto desenvolvido para disciplina SSC0250 - Computação Gráfica.
+
+### Alunos:
+*  **Fábio Verardino de Oliveira** - 12674547
+*  **Rafael Sartori Vantin** - 12543353
+
+## Introdução
+
+O projeto conta com a representação gráfica de um terreno externo, com grama e areia. Além disso, temos uma torre de madeira e uma casa, que conta com um abacaxi, um gato e uma caveira.
---
+++ models.py
@@ -0,0 +1,101 @@
+models_info = [
+    {
+        'obj_filename': 'caixa/caixa.obj',
+        'texture_filename': 'caixa/caixa.jpg',
+
+        'draw_type': 'TRIANGLE',
+        
+        'angle': 0.0,
+        'translation': [0.0, 0.0, 15.0],
+        'rotation': [0.0, 0.0, 1.0],
+        'scale': [1.0, 1.0, 1.0],
+    },
+    {
+        'obj_filename': 'sky/sky.obj',
+        'texture_filename': 'sky/sky.png',
+
+        'draw_type': 'TRIANGLE',
+        
+        'angle': 0.0,
+        'translation': [0.0, 0.0, 0.0],
+        'rotation': [0.0, 1.0, 0.0],
+        'scale': [3.0, 3.0, 3.0],
+    },
+    {
+        'obj_filename': 'terrain/terreno.obj',
+        'texture_filename': 'terrain/grass.jpg',
+
+        'draw_type': 'TRIANGLE',
+        
+        'angle': 0.0,
+        'translation': [0.0, -1.01, 0.0],
+        'rotation': [0.0, 0.0, 1.0],
+        'scale': [200.0, 200.0, 200.0],
+    },
+    {
+        'obj_filename': 'terrain/terreno.obj',
+        'texture_filename': 'terrain/sand.jpeg',
+
+        'draw_type': 'TRIANGLE',
+        
+        'angle': 0.0,
+        'translation': [20.0, -1, 0.0],
+        'rotation': [0.0, 0.0, 1.0],
+        'scale': [10.0, 10.0, 10.0],
+    },
+    {
+        'obj_filename': 'skull/skull.obj',
+        'texture_filename': 'skull/skull.jpg',
+
+        'draw_type': 'QUADS',
+        
+        'angle': 0.0,
+        'translation': [30.0, 4, 0.0],
+        'rotation': [1.0, 1.0, 1.0],
+        'scale': [0.3, 0.3, 0.3],
+    },
+    {
+        'obj_filename': 'pineapple/pineapple.obj',
+        'texture_filename': 'pineapple/pineapple.jpg',
+
+        'draw_type': 'QUADS',
+        
+        'angle': 0.0,
+        'translation': [10.0, 4, 10.0],
+        'rotation': [1.0, 1.0, 1.0],
+        'scale': [0.3, 0.3, 0.3],
+    },
+    {
+        'obj_filename': 'cat/cat.obj',
+        'texture_filename': 'cat/cat.jpg',
+
+        'draw_type': 'QUADS',
+        
+        'angle': 0.0,
+        'translation': [30.0, 4, 5.0],
+        'rotation': [1.0, 1.0, 1.0],
+        'scale': [0.3, 0.3, 0.3],
+    },
+    # {
+    #     'obj_filename': 'farmhouse/farmhouse.obj',
+    #     'texture_filename': 'farmhouse/farmhouse.jpg',
+
+    #     'draw_type': 'QUADS',
+        
+    #     'angle': 0.0,
+    #     'translation': [10.0, 4, 10.0],
+    #     'rotation': [1.0, 1.0, 1.0],
+    #     'scale': [0.3, 0.3, 0.3],
+    # },
+    {
+        'obj_filename': 'watchtower/watchtower.obj',
+        'texture_filename': 'watchtower/watchtower.jpg',
+
+        'draw_type': 'QUADS',
+        
+        'angle': 0.0,
+        'translation': [10.0, 4, 10.0],
+        'rotation': [1.0, 1.0, 1.0],
+        'scale': [0.3, 0.3, 0.3],
+    },
+]
