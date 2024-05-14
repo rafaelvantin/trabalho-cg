e99475ad3bfcd91cded5a4a2317d92a47d905622
Fabio Verardino <fabio.verardino@liven.tech>
2024-05-14 16:37:17 -0300

    refactor de algumas funcoes e models_info vira dict

--- models.py
+++ models.py
@@ -1,101 +1,92 @@
-models_info = [
-    {
-        'obj_filename': 'caixa/caixa.obj',
-        'texture_filename': 'caixa/caixa.jpg',
-
-        'draw_type': 'TRIANGLE',
-        
-        'angle': 0.0,
-        'translation': [0.0, 0.0, 15.0],
-        'rotation': [0.0, 0.0, 1.0],
-        'scale': [1.0, 1.0, 1.0],
+models_info = {
+    "box": {
+        "obj_filename": "caixa/caixa.obj",
+        "texture_filename": "caixa/caixa.jpg",
+        "draw_type": "TRIANGLE",
+        "angle": 0.0,
+        "translation": [0.0, 0.0, 15.0],
+        "rotation": [0.0, 0.0, 1.0],
+        "scale": [1.0, 1.0, 1.0],
     },
-    {
-        'obj_filename': 'sky/sky.obj',
-        'texture_filename': 'sky/sky.png',
-
-        'draw_type': 'TRIANGLE',
-        
-        'angle': 0.0,
-        'translation': [0.0, 0.0, 0.0],
-        'rotation': [0.0, 1.0, 0.0],
-        'scale': [3.0, 3.0, 3.0],
+    "skybox": {
+        "obj_filename": "sky/sky.obj",
+        "texture_filename": "sky/sky.png",
+        "draw_type": "TRIANGLE",
+        "angle": 0.0,
+        "translation": [0.0, 0.0, 0.0],
+        "rotation": [0.0, 1.0, 0.0],
+        "scale": [3.0, 3.0, 3.0],
     },
-    {
-        'obj_filename': 'terrain/terreno.obj',
-        'texture_filename': 'terrain/grass.jpg',
-
-        'draw_type': 'TRIANGLE',
-        
-        'angle': 0.0,
-        'translation': [0.0, -1.01, 0.0],
-        'rotation': [0.0, 0.0, 1.0],
-        'scale': [200.0, 200.0, 200.0],
+    "terrain_grass": {
+        "obj_filename": "terrain/terreno.obj",
+        "texture_filename": "terrain/grass.jpg",
+        "draw_type": "TRIANGLE",
+        "angle": 0.0,
+        "translation": [0.0, -1.01, 0.0],
+        "rotation": [0.0, 0.0, 1.0],
+        "scale": [200.0, 200.0, 200.0],
     },
-    {
-        'obj_filename': 'terrain/terreno.obj',
-        'texture_filename': 'terrain/sand.jpeg',
-
-        'draw_type': 'TRIANGLE',
-        
-        'angle': 0.0,
-        'translation': [20.0, -1, 0.0],
-        'rotation': [0.0, 0.0, 1.0],
-        'scale': [10.0, 10.0, 10.0],
+    "terrain_sand": {
+        "obj_filename": "terrain/terreno.obj",
+        "texture_filename": "terrain/sand.jpeg",
+        "draw_type": "TRIANGLE",
+        "angle": 0.0,
+        "translation": [20.0, -1, 0.0],
+        "rotation": [0.0, 0.0, 1.0],
+        "scale": [10.0, 10.0, 10.0],
     },
-    {
-        'obj_filename': 'skull/skull.obj',
-        'texture_filename': 'skull/skull.jpg',
-
-        'draw_type': 'QUADS',
-        
-        'angle': 0.0,
-        'translation': [30.0, 4, 0.0],
-        'rotation': [1.0, 1.0, 1.0],
-        'scale': [0.3, 0.3, 0.3],
+    "small_skull": {
+        "obj_filename": "skull/skull.obj",
+        "texture_filename": "skull/skull.jpg",
+        "draw_type": "QUADS",
+        "angle": 0.0,
+        "translation": [30.0, 4, 0.0],
+        "rotation": [1.0, 1.0, 1.0],
+        "scale": [0.3, 0.3, 0.3],
     },
-    {
-        'obj_filename': 'pineapple/pineapple.obj',
-        'texture_filename': 'pineapple/pineapple.jpg',
-
-        'draw_type': 'QUADS',
-        
-        'angle': 0.0,
-        'translation': [10.0, 4, 10.0],
-        'rotation': [1.0, 1.0, 1.0],
-        'scale': [0.3, 0.3, 0.3],
+    "big_skull": {
+        "obj_filename": "skull/skull.obj",
+        "texture_filename": "skull/skull.jpg",
+        "draw_type": "QUADS",
+        "angle": 0.0,
+        "translation": [30.0, 4, 0.0],
+        "rotation": [1.0, 1.0, 1.0],
+        "scale": [1.0, 1.0, 1.0],
     },
-    {
-        'obj_filename': 'cat/cat.obj',
-        'texture_filename': 'cat/cat.jpg',
-
-        'draw_type': 'QUADS',
-        
-        'angle': 0.0,
-        'translation': [30.0, 4, 5.0],
-        'rotation': [1.0, 1.0, 1.0],
-        'scale': [0.3, 0.3, 0.3],
+    "pineapple": {
+        "obj_filename": "pineapple/pineapple.obj",
+        "texture_filename": "pineapple/pineapple.jpg",
+        "draw_type": "QUADS",
+        "angle": 0.0,
+        "translation": [10.0, 4, 10.0],
+        "rotation": [1.0, 1.0, 1.0],
+        "scale": [0.3, 0.3, 0.3],
+    },
+    "cat": {
+        "obj_filename": "cat/cat.obj",
+        "texture_filename": "cat/cat.jpg",
+        "draw_type": "QUADS",
+        "angle": 0.0,
+        "translation": [30.0, 4, 5.0],
+        "rotation": [1.0, 1.0, 1.0],
+        "scale": [0.3, 0.3, 0.3],
     },
     # {
     #     'obj_filename': 'farmhouse/farmhouse.obj',
     #     'texture_filename': 'farmhouse/farmhouse.jpg',
-
     #     'draw_type': 'QUADS',
-        
     #     'angle': 0.0,
     #     'translation': [10.0, 4, 10.0],
     #     'rotation': [1.0, 1.0, 1.0],
     #     'scale': [0.3, 0.3, 0.3],
     # },
-    {
-        'obj_filename': 'watchtower/watchtower.obj',
-        'texture_filename': 'watchtower/watchtower.jpg',
-
-        'draw_type': 'QUADS',
-        
-        'angle': 0.0,
-        'translation': [10.0, 4, 10.0],
-        'rotation': [1.0, 1.0, 1.0],
-        'scale': [0.3, 0.3, 0.3],
+    "watchtower": {
+        "obj_filename": "watchtower/watchtower.obj",
+        "texture_filename": "watchtower/watchtower.jpg",
+        "draw_type": "QUADS",
+        "angle": 0.0,
+        "translation": [10.0, 4, 10.0],
+        "rotation": [1.0, 1.0, 1.0],
+        "scale": [0.3, 0.3, 0.3],
     },
-]
+}
