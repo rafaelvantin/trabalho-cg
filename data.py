7b6b6cb661756613899f5f046423723e31e30436
Fabio Verardino <fabio.verardino@liven.tech>
2024-06-25 19:57:33 -0300

    final version

--- data.py
+++ data.py
@@ -1,15 +1,43 @@
 light_sources = {
     "external": {
-        "position": [0, 20, 0],
-        "color": [0.7, 1.0, 1.0],
+        "position": [45, 33, 15],
+        "color": [0.93, 0.92, 1.0],
+        "rotation_data": {
+            "center": [65, 33, 15],
+            "radius": 22,
+            "speed": 0.007,
+            "angle": 0,
+        },
     },
     "internal": {
-        "position": [-26, 2, -18],
-        "color": [1.0, 1.0, 0.7],
+        "position": [-28, 3.5, -19],
+        "color": [1.0, 0.83, 0.42],
     },
 }
 
 models_repo = {
+    "internal_light_model": {
+        "obj_filename": "caixa/caixa.obj",
+        "texture_filename": "caixa/white.jpg",
+        "draw_type": "TRIANGLE",
+        "translation": light_sources["internal"]["position"],
+        "scale": [0.1, 0.1, 0.1],
+        "angles": [0, 0, 0],
+        "light_source": "internal",
+        "light_coefs": [1.0, 1.0, 1.0, 1.0],
+        "follow_light": True,
+    },
+    "external_light_model": {
+        "obj_filename": "caixa/caixa.obj",
+        "texture_filename": "caixa/white.jpg",
+        "draw_type": "TRIANGLE",
+        "translation": light_sources["external"]["position"],
+        "scale": [0.1, 0.1, 0.1],
+        "angles": [0, 0, 0],
+        "light_source": "external",
+        "light_coefs": [1.0, 1.0, 1.0, 1.0],
+        "follow_light": True,
+    },
     "box": {
         "obj_filename": "caixa/caixa.obj",
         "texture_filename": "caixa/caixa.jpg",
@@ -38,7 +66,7 @@ models_repo = {
         "scale": [200, 1, 200],
         "angles": [0, 0, 0],
         "light_source": "external",
-        "light_coefs": [1.0, 1.0, 1.0, 1.0],
+        "light_coefs": [1.0, 1.0, 0.7, 30.0],
     },
     "terrain_sand": {
         "obj_filename": "terrain/terrain_adjusted.obj",
@@ -48,7 +76,7 @@ models_repo = {
         "scale": [50, 1, 50],
         "angles": [0, 0, 0],
         "light_source": "external",
-        "light_coefs": [1.0, 1.0, 1.0, 1.0],
+        "light_coefs": [1.0, 1.0, 0.7, 30.0],
     },
     "small_skull": {
         "obj_filename": "skull/skull.obj",
@@ -68,7 +96,7 @@ models_repo = {
         "scale": [0.7, 0.7, 0.7],
         "angles": [-90, 0, 0],
         "light_source": "external",
-        "light_coefs": [1.0, 1.0, 1.0, 1.0],
+        "light_coefs": [0.5, 0.7, 1.0, 5.0],
     },
     "pineapple": {
         "obj_filename": "pineapple/pineapple.obj",
@@ -88,7 +116,7 @@ models_repo = {
         "scale": [0.042, 0.042, 0.042],
         "angles": [-90, 100, 0],
         "light_source": "external",
-        "light_coefs": [1.0, 1.0, 1.0, 1.0],
+        "light_coefs": [0.9, 0.8, 0.3, 40.0],
     },
     "watchtower": {
         "obj_filename": "watchtower/watchtower.obj",
@@ -98,7 +126,7 @@ models_repo = {
         "scale": [2, 3.5, 2],
         "angles": [0, 0, 0],
         "light_source": "external",
-        "light_coefs": [1.0, 1.0, 1.0, 1.0],
+        "light_coefs": [0.8, 0.8, 0.5, 25.0],
     },
     "farmhouse": {
         "obj_filename": "farmhouse/farmhouse_adjusted.obj",
@@ -108,6 +136,6 @@ models_repo = {
         "scale": [0.5, 0.5, 0.5],
         "angles": [0, -135, 0],
         "light_source": "internal",
-        "light_coefs": [0.3, 0.5, 1.0, 5.0],
+        "light_coefs": [0.7, 0.7, 1.0, 15.0],
     },
 }
